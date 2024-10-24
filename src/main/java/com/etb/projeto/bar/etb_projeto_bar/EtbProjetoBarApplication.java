7d4495ecb7f042b1d92a056daa8063fe4d719844
jvscJoao <joaov1500@gmail.com>
2024-10-24 15:27:58 -0300

    first commit

---
+++ src/main/java/com/etb/projeto/bar/etb_projeto_bar/EtbProjetoBarApplication.java
@@ -0,0 +1,13 @@
+package com.etb.projeto.bar.etb_projeto_bar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EtbProjetoBarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EtbProjetoBarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/etb/projeto/bar/etb_projeto_bar/EtbProjetoBarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.etb.projeto.bar.etb_projeto_bar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EtbProjetoBarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
